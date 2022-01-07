d7e9beef4fdb32b3c8fbd52e382656266cf228b0
KhasanovR <r.khasanov97@gmail.com>
2022-01-07 16:15:03 +0500

    add repositories for Country and Region

---
+++ src/main/java/com/example/pharmacy/core/repository/RegionRepository.java
@@ -0,0 +1,15 @@
+package com.example.pharmacy.core.repository;
+
+import com.example.pharmacy.core.model.Country;
+import com.example.pharmacy.core.model.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+    Optional<Region> findById(Long id);
+    Region findRegionByIdAnd(Long id);
+    Collection<Region> findRegionsByCountry(Country country);
+    Collection<Region> findRegionsByParent(Region parent);
+}
