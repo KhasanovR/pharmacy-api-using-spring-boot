76a17c377c8d832ad0fd0768e7cde842f4054c64
KhasanovR <r.khasanov97@gmail.com>
2022-01-07 19:58:43 +0500

    fix ManufacturerRepository(change InternationalName to Manufacturer)

--- src/main/java/com/example/pharmacy/reference_book/repository/ManufacturerRepository.java
+++ src/main/java/com/example/pharmacy/reference_book/repository/ManufacturerRepository.java
@@ -1,15 +1,15 @@
 package com.example.pharmacy.reference_book.repository;
 
-import com.example.pharmacy.reference_book.model.InternationalName;
+import com.example.pharmacy.reference_book.model.Manufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ManufacturerRepository extends JpaRepository<InternationalName, Long> {
-    Optional<InternationalName> findById(Long id);
-    Optional<InternationalName> findByName(String name);
-    InternationalName findInternationalNameById(Long id);
-    InternationalName findInternationalNameByName(String name);
-    void deleteInternationalNameById(Long id);
-    void deleteInternationalNameByName(String name);
+public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long> {
+    Optional<Manufacturer> findById(Long id);
+    Optional<Manufacturer> findByName(String name);
+    Manufacturer findManufacturerById(Long id);
+    Manufacturer findManufacturerByName(String name);
+    void deleteManufacturerById(Long id);
+    void deleteManufacturerByName(String name);
 }
