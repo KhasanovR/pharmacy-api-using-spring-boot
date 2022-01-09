9740382c1b167ef3db5eec386cce665108f01542
KhasanovR <r.khasanov97@gmail.com>
2022-01-09 08:56:49 +0500

    added logging and current user fetching in Country & Region controllers

--- src/main/java/com/example/pharmacy/core/controller/CountryController.java
+++ src/main/java/com/example/pharmacy/core/controller/CountryController.java
@@ -1,10 +1,14 @@
 package com.example.pharmacy.core.controller;
 
+import com.example.pharmacy.account.model.AppUser;
+import com.example.pharmacy.account.service.AppUserService;
 import com.example.pharmacy.core.model.Country;
 import com.example.pharmacy.core.service.CountryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -17,42 +21,51 @@ import java.util.Collection;
 public class CountryController {
 
     private final CountryService countryService;
+    private final AppUserService appUserService;
 
     @Autowired
-    public CountryController(CountryService countryService) {
+    public CountryController(CountryService countryService, AppUserService appUserService) {
         this.countryService = countryService;
+        this.appUserService = appUserService;
     }
 
     @GetMapping("/all")
     public ResponseEntity<?> findCountries() {
         Collection<Country> countries = this.countryService.getCountries();
-        log.info("listing: {}", countries);
+        log.info("listing countries: {}", countries);
         return ResponseEntity.ok().body(countries);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findCountry(@PathVariable("id") Long id) {
         Country country = this.countryService.findCountryById(id);
-        log.info("listing: {}", country);
+        log.info("listing country: {}", country);
         return ResponseEntity.ok().body(country);
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> registerCountry(@RequestBody Country country) {
-        Country savedCountry = this.countryService.saveCountry(country);
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/countries/").toUriString()
+    public ResponseEntity<?> registerCountry(@RequestBody Country country, @AuthenticationPrincipal UserDetails userDetails) {
+        log.info("adding country: {}", country);
+        String username = userDetails.getUsername();
+        AppUser user = appUserService.getUser(username);
+        Country savedCountry = this.countryService.saveCountry(country, user);
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/countries/").toUriString()
                 + savedCountry.getId());
         return ResponseEntity.created(uri).body(savedCountry);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<?> updateCountry(@RequestBody Country country) {
-        Country updatedCountry = this.countryService.updateCountry(country);
+    public ResponseEntity<?> updateCountry(@RequestBody Country country, @AuthenticationPrincipal UserDetails userDetails) {
+        log.info("updating country: {}", country);
+        String username = userDetails.getUsername();
+        AppUser user = appUserService.getUser(username);
+        Country updatedCountry = this.countryService.updateCountry(country, user);
         return ResponseEntity.ok().body(updatedCountry);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> removeCountry(@PathVariable("id") Long id){
+        log.info("deleting country with id: {}", id);
         this.countryService.deleteCountryById(id);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/example/pharmacy/core/controller/RegionController.java
+++ src/main/java/com/example/pharmacy/core/controller/RegionController.java
@@ -1,10 +1,14 @@
 package com.example.pharmacy.core.controller;
 
+import com.example.pharmacy.account.model.AppUser;
+import com.example.pharmacy.account.service.AppUserService;
 import com.example.pharmacy.core.model.Region;
 import com.example.pharmacy.core.service.RegionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -17,42 +21,51 @@ import java.util.Collection;
 public class RegionController {
 
     private final RegionService RegionService;
+    private final AppUserService appUserService;
 
     @Autowired
-    public RegionController(RegionService RegionService) {
+    public RegionController(RegionService RegionService, AppUserService appUserService) {
         this.RegionService = RegionService;
+        this.appUserService = appUserService;
     }
 
     @GetMapping("/all")
     public ResponseEntity<?> findRegions() {
-        Collection<Region> regions = this.RegionService.getCountries();
-        log.info("listing: {}", regions);
+        Collection<Region> regions = this.RegionService.getRegions();
+        log.info("listing regions: {}", regions);
         return ResponseEntity.ok().body(regions);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findRegion(@PathVariable("id") Long id) {
         Region Region = this.RegionService.findRegion(id);
-        log.info("listing: {}", Region);
+        log.info("listing region: {}", Region);
         return ResponseEntity.ok().body(Region);
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> registerRegion(@RequestBody Region Region) {
-        Region savedRegion = this.RegionService.saveRegion(Region);
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/regions/").toUriString()
+    public ResponseEntity<?> registerRegion(@RequestBody Region region, @AuthenticationPrincipal UserDetails userDetails) {
+        log.info("adding region: {}", region);
+        String username = userDetails.getUsername();
+        AppUser user = appUserService.getUser(username);
+        Region savedRegion = this.RegionService.saveRegion(region, user);
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/regions/").toUriString()
                 + savedRegion.getId());
         return ResponseEntity.created(uri).body(savedRegion);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<?> updateRegion(@RequestBody Region Region) {
-        Region updatedRegion = this.RegionService.updateRegion(Region);
+    public ResponseEntity<?> updateRegion(@RequestBody Region region, @AuthenticationPrincipal UserDetails userDetails) {
+        log.info("updating region: {}", region);
+        String username = userDetails.getUsername();
+        AppUser user = appUserService.getUser(username);
+        Region updatedRegion = this.RegionService.updateRegion(region, user);
         return ResponseEntity.ok().body(updatedRegion);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> removeRegion(@PathVariable("id") Long id){
+        log.info("deleting region with id: {}", id);
         this.RegionService.deleteRegionById(id);
         return ResponseEntity.ok().build();
     }
