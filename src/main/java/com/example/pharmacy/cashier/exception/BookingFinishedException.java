8783f45e883fdf3c6e9c30437cd4e95f4603227f
KhasanovR <r.khasanov97@gmail.com>
2022-01-09 17:17:24 +0500

    add additional exceptions for cashier

---
+++ src/main/java/com/example/pharmacy/cashier/exception/BookingFinishedException.java
@@ -0,0 +1,7 @@
+package com.example.pharmacy.cashier.exception;
+
+public class BookingFinishedException extends RuntimeException {
+    public BookingFinishedException(String message) {
+        super(message);
+    }
+}
